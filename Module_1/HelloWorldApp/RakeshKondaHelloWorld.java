28790e707118775121d1a0fb9b1b0e8becf9b1e7
Rakesh Konda <rkonda@my365.bellevue.edu>
2021-06-14 00:34:49 +0530

    Added a new file Module_1/HelloWorldApp/RakeshKondaHelloWorld.java

---
+++ Module_1/HelloWorldApp/RakeshKondaHelloWorld.java
@@ -0,0 +1,18 @@
+/* Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
+Comprehensive Version (12th ed.). Pearson Education, Inc.
+ */
+package Module_1.HelloWorldApp;
+
+public class RakeshKondaHelloWorld
+{
+    public static void main(String args[]) 
+    {
+        /*
+        *Creating String variable firstName & assigning a value
+        *Creating String variable lastName & assigning a value
+        */
+        String firstName = "Rakesh";
+        String lastName = "Konda";
+        System.out.println("Hello World from "+ firstName +" "+ lastName +"." ); //printing output
+    } // end main 
+} // end RakeshKondaHelloWorld
